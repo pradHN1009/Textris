9929288a2ae22b838eabb0c82096f6788ba91299
pradHN1009 <pradyumnahn@gmail.com>
2023-10-15 23:08:34 +0530

    Added about component and used react router to navigate to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 import React, { useState } from 'react'
 import Alert from './Components/Alert';
+import About from './Components/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -32,12 +34,17 @@ function App() {
 
   return (
     <>
+      <Router>
       <Navbar title="Textris" aboutText="About" mode = {mode} toggleMode = {toggleMode}/>
       <Alert alert = {alert}/>
       <div className="container my-3">
-        <Textform heading = "Enter text" mode={mode} alertFunc={displayAlert}/>
+      <Routes>
+        <Route exact path="/" element={<Textform heading = "Enter text" mode={mode} alertFunc={displayAlert}/>}/>
+          <Route index element={<Textform heading = "Enter text" mode={mode} alertFunc={displayAlert}/>}/>
+          <Route exact path="/about" element={<About />} />
+      </Routes>
       </div>
-      
+    </Router>
     </>
   );
 }
---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+    <p>This is a simple app that lets you perform some operations on text entered. You can convert the text to uppercase or lowecase, you can recite the text entered, or copy it as well. This app supports dark mode as well to ease your eye strain.</p>    
+    </div>
+  )
+}
+
+export default About
