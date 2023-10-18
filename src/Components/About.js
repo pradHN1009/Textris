090669d4cbf64e2b9979de999a7c6112bd87d231
pradHN1009 <pradyumnahn@gmail.com>
2023-10-18 20:06:19 +0530

    Added prop types and default props to About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 function About(props) {
   return (
     <div className = "row">
         <div className={`col-12 text-${props.mode==="dark"?"light":"black"}`}><p>This is a simple app that lets you perform some operations on text entered. You can convert the text to uppercase or lowecase, you can recite the text entered, or copy it as well. This app supports dark mode as well to ease your eye strain.</p>    </div>
     </div>
   )
+}
+
+About.propTypes = {
+    mode : PropTypes.string
+}
+About.defaultProps = {
+    mode : "light"
 }
 
 export default About
